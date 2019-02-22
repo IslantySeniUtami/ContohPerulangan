e291980ea128c0c255e3fbf21b730269557e8226
IslantySeniUtami <46374176+IslantySeniUtami@users.noreply.github.com>
2019-02-22 08:09:37 +0700

    Add files via upload

---
+++ ContohPerulangan.java
@@ -0,0 +1,11 @@
+package contohperulangan;
+public class ContohPerulangan {
+    public static void main(String[] args) {
+        
+    for (int hitungan = 0; hitungan <= 1000; hitungan++) {
+        System.out.println("Petani kode");
+    }
+       
+    }
+    
+}
